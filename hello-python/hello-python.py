41841331e2bfb36f1ce1a340b186bb3e14ea66ce
William Collins <26700607+wcollins@users.noreply.github.com>
2026-04-27 16:23:52 -0400

    feat: add iagctl hello-world services (#1)
    * chore: extend gitignore for python and opentofu
    
    * feat: add iagctl service import manifest
    
    * feat: add ansible hello-world service
    
    * feat: add python hello-world service
    
    * feat: add opentofu hello-world service

---
+++ hello-python/hello-python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import argparse
+
+def main():
+    # Create argument parser to receive command-line arguments from iagctl
+    parser = argparse.ArgumentParser(description='Python hello world with two string variables')
+    parser.add_argument('--first_string', type=str, default='Hello',
+                        help='First string to print')
+    parser.add_argument('--second_string', type=str, default='World',
+                        help='Second string to print')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Print the two strings
+    print(args.first_string)
+    print(args.second_string)
+
+if __name__ == "__main__":
+    main()
